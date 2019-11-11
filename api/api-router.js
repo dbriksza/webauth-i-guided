1e8f6a535a3cebe898663051325f7c5438990db1
dbriksza <dbriksza@gmail.com>
2019-11-11 12:36:06 -0500

    working on hashing passwords

--- api/api-router.js
+++ api/api-router.js
@@ -1,13 +1,20 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+const bcrypt = require("bcryptjs");
 
-router.get('/', (req, res) => {
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
+
+router.get("/", (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post("/hash", (req, res) => {
+  let salt = bcrypt.genSalt(15);
+  let hash = bcrypt.hashSync(req.password, salt);
 });
 
 module.exports = router;
